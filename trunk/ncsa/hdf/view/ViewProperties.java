cacb877ebe1ffb0390450974b3c6a050ade38d9f
xcao <xcao@a541ca71-9318-0410-8328-a4f699e697c0>
2002-07-23 16:38:06 +0000

    *** empty log message ***

--- trunk/ncsa/hdf/view/ViewProperties.java
+++ trunk/ncsa/hdf/view/ViewProperties.java
@@ -30,7 +30,7 @@ import java.net.URLClassLoader;
 public class ViewProperties extends Properties
 {
     /** the version of the HDFViewer */
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
 
     /** the local property file name */
     public static final String USER_PROPS = "hdfview.props";
