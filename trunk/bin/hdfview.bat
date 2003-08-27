57075deee994f147eafc58c4e5ec48765d13f9d7
xcao <xcao@a541ca71-9318-0410-8328-a4f699e697c0>
2003-08-27 19:14:35 +0000

    *** empty log message ***

---
+++ trunk/bin/hdfview.bat
@@ -0,0 +1,24 @@
+@ECHO OFF
+
+@REM =======================================================================
+@REM                 JHV run batch file for Windows 95/98/NT
+@REM =======================================================================
+
+
+@REM To run this Java-based HDF Viewer (JHV), You need jdk1.1.x. 
+
+@REM set up your jhv home directory, i.e. where you installed the JHV
+@SET HDFVIEW_HOME=I:\ModularHDFView
+@SET JAVAHOME=D:\Java\jdk1.4.1
+
+@REM Do not make changes under this line unless you know what you are doing.
+@REM =======================================================================
+
+SET PATH=%HDFVIEW_HOME%\lib\win;%PATH%
+
+set CLASSPATH=%HDFVIEW_HOME%/lib/jhdf.jar;%HDFVIEW_HOME%/lib/jhdf5.jar;%HDFVIEW_HOME%/lib/jhdfobj.jar
+set CLASSPATH=%CLASSPATH%;%HDFVIEW_HOME%/lib/jhdf4obj.jar;%HDFVIEW_HOME%/lib/jhdf5obj.jar;%HDFVIEW_HOME%/lib/jhdfview.jar
+
+%JAVAHOME%\bin\java -mx512m -classpath %CLASSPATH% ncsa.hdf.view.HDFView -root %HDFVIEW_HOME%
+pause
+:END
