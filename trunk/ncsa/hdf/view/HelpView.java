5b012365156bda35da581329d3d0711e87ac2e62
xcao <xcao@a541ca71-9318-0410-8328-a4f699e697c0>
2004-05-21 21:04:25 +0000

    *** empty log message ***

---
+++ trunk/ncsa/hdf/view/HelpView.java
@@ -0,0 +1,33 @@
+/****************************************************************************
+ * NCSA HDF                                                                 *
+ * National Computational Science Alliance                                   *
+ * University of Illinois at Urbana-Champaign                               *
+ * 605 E. Springfield, Champaign IL 61820                                   *
+ *                                                                          *
+ * For conditions of distribution and use, see the accompanying             *
+ * hdf-java/COPYING file.                                                   *
+ *                                                                          *
+ ****************************************************************************/
+
+package ncsa.hdf.view;
+
+import ncsa.hdf.object.*;
+
+/**
+ *
+ * <p>Title: ncsa.hdf.view.DataView</p>
+ * <p>The helpview interface for displaying user help information</p>
+ * <p>Company: National Computational Science Alliance</p>
+ * @author Peter X. Cao
+ * @version 1.0
+ */
+public abstract interface HelpView {
+    /** display help informaion */
+    public abstract void show();
+
+    /** Returns the HelpView's label, which is used to displayed in the HDFVIew help menu. */
+    public abstract String getLabel();
+
+    /** Returns the action command for this HelpView. */
+    public abstract String getActionCommand();
+}
