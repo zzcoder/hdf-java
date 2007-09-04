8ed999289e1e0f298ea73e112dcf615019684760
aydt <aydt@a541ca71-9318-0410-8328-a4f699e697c0>
2007-09-04 16:30:11 +0000

    Updated copyright, URLs, version, date to reflect no longer @ NCSA.

--- trunk/ncsa/hdf/object/fits/FitsDataset.java
+++ trunk/ncsa/hdf/object/fits/FitsDataset.java
@@ -1,12 +1,15 @@
-/****************************************************************************
- * NCSA HDF                                                                 *
- * National Comptational Science Alliance                                   *
- * University of Illinois at Urbana-Champaign                               *
- * 605 E. Springfield, Champaign IL 61820                                   *
- *                                                                          *
- * For conditions of distribution and use, see the accompanying             *
- * hdf-java/COPYING file.                                                   *
- *                                                                          *
+/*****************************************************************************
+ * Copyright by The HDF Group.                                               *
+ * Copyright by the Board of Trustees of the University of Illinois.         *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of the HDF Java Products distribution.                  *
+ * The full copyright notice, including terms governing use, modification,   *
+ * and redistribution, is contained in the files COPYING and Copyright.html. *
+ * COPYING can be found at the root of the source code distribution tree.    *
+ * Or, see http://hdfgroup.org/products/hdf-java/doc/Copyright.html.         *
+ * If you do not have access to either file, you may request a copy from     *
+ * help@hdfgroup.org.                                                        *
  ****************************************************************************/
 
 package ncsa.hdf.object.fits;
@@ -23,11 +26,11 @@ import nom.tam.fits.*;
  * and operations performed on the scalar dataset
  * <p>
  * The library predefines a modest number of datatypes. For details, read
- * <a href="http://hdf.ncsa.uiuc.edu/HDF5/doc/Datatypes.html">
+ * <a href="http://hdfgroup.org/HDF5/doc/Datatypes.html">
  * The Datatype Interface (H5T)</a>
  * <p>
- * @version 1.0 12/12/2001
- * @author Peter X. Cao, NCSA
+ * @version 1.1 9/4/2007
+ * @author Peter X. Cao
  */
 public class FitsDataset extends ScalarDS
 {
