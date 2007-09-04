d2b4a380721d8bfed1d193bc18b40664320731bd
aydt <aydt@a541ca71-9318-0410-8328-a4f699e697c0>
2007-09-04 15:59:40 +0000

    Updated copyright info, version, date to reflect no longer @ NCSA.

--- trunk/ncsa/hdf/object/h4/H4Datatype.java
+++ trunk/ncsa/hdf/object/h4/H4Datatype.java
@@ -1,12 +1,15 @@
-/****************************************************************************
- * NCSA HDF                                                                 *
- * National Comptational Science Alliance                                   *
- * University of Illinois at Urbana-Champaign                               *
- * 605 E. Springfield, Champaign IL 61820                                   *
- *                                                                          *
- * For conditions of distribution and use, see the accompanying             *
- * hdf-java/COPYING file.                                                   *
- *                                                                          *
+/*****************************************************************************
+ * Copyright by The HDF Group.                                               *
+ * Copyright by the Board of Trustees of the University of Illinois.         *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of the HDF Java Products distribution.                  *
+ * The full copyright notice, including terms governing use, modification,   *
+ * and redistribution, is contained in the files COPYING and Copyright.html. *
+ * COPYING can be found at the root of the source code distribution tree.    *
+ * Or, see http://hdfgroup.org/products/hdf-java/doc/Copyright.html.         *
+ * If you do not have access to either file, you may request a copy from     *
+ * help@hdfgroup.org.                                                        *
  ****************************************************************************/
 
 package ncsa.hdf.object.h4;
@@ -17,13 +20,13 @@ import ncsa.hdf.object.*;
 /**
  * This class defines HDF4 data type characteristics and APIs for a data type.
  * <p>
- * This class provides several methods to convert an HDF4 dataype identifier to
- * a dataype object, and vice versa. A dataype object is described by four basic 
- * fields: datatype class, size, byte order, and sign, while an HDF5 dataype is 
- * presented by a datetype identifier. 
+ * This class provides several methods to convert an HDF4 datatype identifier to
+ * a datatype object, and vice versa. A datatype object is described by four basic 
+ * fields: datatype class, size, byte order, and sign, while an HDF5 datatype is 
+ * presented by a datatype identifier. 
  * <p>
- * @version 1.0 05/07/2002
- * @author Peter X. Cao, NCSA
+ * @version 1.1 9/4/2007
+ * @author Peter X. Cao
  */
 public class H4Datatype extends Datatype
 {
