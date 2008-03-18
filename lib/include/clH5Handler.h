9008ab76c89d613ce063be5bdc088525e35f9fcd
mwan <mwan@a541ca71-9318-0410-8328-a4f699e697c0>
2008-03-18 23:06:42 +0000

    Add code to get iRods/hdf5 working

--- lib/include/clH5Handler.h
+++ lib/include/clH5Handler.h
@@ -13,17 +13,10 @@
  * access to either file, you may request a copy from hdfhelp@ncsa.uiuc.edu. *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-#ifndef _H5HANDLER_H
-#define _H5HANDLER_H
+#ifndef _CLH5HANDLER_H
+#define _CLH5HANDLER_H
 
 #include "rodsClient.h"
-
-struct openedH5File {
-    int fid;	/* the fid of the opened H5File */
-    struct hostElement  *hostTabPtr;	/* host table where the file is 
-					 * located */
-    struct openedH5File *next;    /* link list pointer */
-};
 
 #ifdef __cplusplus
 extern "C" {
@@ -34,17 +27,6 @@ extern "C" {
  */
 int h5ObjRequest(rcComm_t *conn, void *obj, int objID);
 
-/* Server side API:
- *     process request from the client and send result to the client
- */
-#if 0	/* XXXX rm for irods */
-int h5ObjProcess(genFunctType_t functType, int objID, int intInput2,
-char *strInput1, char *strInput2, bytesBuf_t *bytesBufInp,
-bytesBuf_t *bytesBufOut, int maxOutSz);
-int
-addOpenedHdf5File (int fid, struct hostElement *hostTabPtr);
-struct hostElement *getHostByFid (int fid);
-#endif
 #ifdef __cplusplus
 }
 #endif
