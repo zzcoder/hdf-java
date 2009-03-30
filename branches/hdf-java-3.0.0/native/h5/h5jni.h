b74f5ef4fcd7b7c21843dade5e0884858a25c0cc
xcao <xcao@a541ca71-9318-0410-8328-a4f699e697c0>
2009-03-30 21:29:51 +0000

    Add jni c code file (from hdf-java 2.5)

---
+++ branches/hdf-java-3.0.0/native/h5/h5jni.h
@@ -0,0 +1,26 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright by The HDF Group.                                               *
+ * Copyright by the Board of Trustees of the University of Illinois.         *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of HDF Java Products. The full HDF Java copyright       *
+ * notice, including terms governing use, modification, and redistribution,  *
+ * is contained in the file, COPYING.  COPYING can be found at the root of   *
+ * the source code distribution tree. You can also access it online  at      *
+ * http://www.hdfgroup.org/products/licenses.html.  If you do not have       *
+ * access to the file, you may request a copy from help@hdfgroup.org.        *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#ifdef __cplusplus
+#define ENVPTR (env)
+#define ENVPAR
+#else
+#define ENVPTR (*env)
+#define ENVPAR env,
+#endif
+
+extern jboolean h5JNIFatalError( JNIEnv *env, char *functName);
+extern jboolean h5nullArgument( JNIEnv *env, char *functName);
+extern jboolean h5libraryError( JNIEnv *env );
+extern jboolean h5raiseException( JNIEnv *env, char *exception, char *message);
+
