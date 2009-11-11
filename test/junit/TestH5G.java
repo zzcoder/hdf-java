1912425789665cb619a4702086dec0cf1b10ac27
byrn <byrn@a541ca71-9318-0410-8328-a4f699e697c0>
2009-11-11 22:22:26 +0000

    Correct Indentation

--- test/junit/TestH5G.java
+++ test/junit/TestH5G.java
@@ -16,162 +16,162 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestH5G {
-    private static final boolean is16 = H5.isAPI16;
-    private static final String H5_FILE = "test.h5";
-    private static final String[] GROUPS = {"/G1", "/G1/G11", "/G1/G12", 
-        "/G1/G11/G111", "/G1/G11/G112", "/G1/G11/G113", "/G1/G11/G114"};
-    int H5fid = -1;
+  private static final boolean is16 = H5.isAPI16;
+  private static final String H5_FILE = "test.h5";
+  private static final String[] GROUPS = {"/G1", "/G1/G11", "/G1/G12", 
+    "/G1/G11/G111", "/G1/G11/G112", "/G1/G11/G113", "/G1/G11/G114"};
+  int H5fid = -1;
 
-    private final int _createGroup(int fid, String name) {
-      int gid = -1;
+  private final int _createGroup(int fid, String name) {
+    int gid = -1;
+    try {
+      if (is16)
+        gid = H5.H5Gcreate(fid, name, 0);
+      else
+        gid = H5.H5Gcreate2(fid, name, HDF5Constants.H5P_DEFAULT, HDF5Constants.H5P_DEFAULT, HDF5Constants.H5P_DEFAULT);
+    }
+    catch (Throwable err) {
+      err.printStackTrace();
+      fail("H5.H5Gcreate: "+err);
+    }
+
+    return gid;
+  }
+
+  private final int _openGroup(int fid, String name) {
+    int gid = -1;
+    try {
+      if (is16)
+        gid = H5.H5Gopen(fid, name);
+      else
+        gid = H5.H5Gopen2(fid, name, HDF5Constants.H5P_DEFAULT);;
+    } 
+    catch (Throwable err) {
+      gid = -1;
+      err.printStackTrace();
+      fail("H5.H5Gcreate: "+err);
+    }
+
+    return gid;
+  }
+
+  private final void _deleteFile(String filename)
+  {
+    File file = new File(filename);
+
+    if (file.exists()) {
       try {
-        if (is16)
-          gid = H5.H5Gcreate(fid, name, 0);
-        else
-          gid = H5.H5Gcreate2(fid, name, HDF5Constants.H5P_DEFAULT, HDF5Constants.H5P_DEFAULT, HDF5Constants.H5P_DEFAULT);
+        file.delete();
+      } 
+      catch (SecurityException e) {
+        ;//e.printStackTrace();
+      }
+    }
+  }
+
+  @Before
+  public void createH5file() throws HDF5LibraryException, NullPointerException {
+    H5fid = H5.H5Fcreate(H5_FILE, HDF5Constants.H5F_ACC_TRUNC, 
+        HDF5Constants.H5P_DEFAULT, HDF5Constants.H5P_DEFAULT);
+
+    int gid = -1;
+
+    for (int i=0; i<GROUPS.length; i++) {
+      gid = _createGroup(H5fid, GROUPS[i]);
+      assertTrue(gid > 0);
+      try { H5.H5Gclose(gid); } catch (Exception ex) {}
+    }
+
+    H5.H5Fflush(H5fid, HDF5Constants.H5F_SCOPE_LOCAL);
+  }
+
+  @After
+  public void deleteH5file() throws HDF5LibraryException {
+    if(H5fid > 0) {
+      H5.H5Fclose(H5fid);
+    }
+    _deleteFile(H5_FILE);
+  }
+
+  @Test
+  public void testH5Gopen() {
+    for (int i=0; i<GROUPS.length; i++) {
+      int gid = _openGroup(H5fid, GROUPS[i]);
+      assertTrue(gid>0);
+      try { H5.H5Gclose(gid); } catch (Exception ex) {}
+    }
+  }
+
+  @Test
+  public void testH5Gget_create_plist() {
+    int gid=-1;
+    int pid=-1;
+
+    for (int i=0; i<GROUPS.length; i++) {
+      gid = _openGroup(H5fid, GROUPS[i]);
+      assertTrue(gid>0);
+
+      try { 
+        pid = H5.H5Gget_create_plist(gid); 
       }
       catch (Throwable err) {
         err.printStackTrace();
-        fail("H5.H5Gcreate: "+err);
+        fail("H5.H5Gget_create_plist: "+err);
       }
+      assertTrue(pid>0);
 
-      return gid;
+      try { H5.H5Gclose(gid); } catch (Exception ex) {}
     }
-    
-    private final int _openGroup(int fid, String name) {
-        int gid = -1;
-        try {
-            if (is16)
-                gid = H5.H5Gopen(fid, name);
-            else
-                gid = H5.H5Gopen2(fid, name, HDF5Constants.H5P_DEFAULT);;
-        } 
-        catch (Throwable err) {
-          gid = -1;
-          err.printStackTrace();
-          fail("H5.H5Gcreate: "+err);
-        }
-        
-        return gid;
-    }
+  }
 
-    private final void _deleteFile(String filename)
-    {
-      File file = new File(filename);
+  @Test
+  public void testH5Gget_info() {
+    H5G_info_t info = null;
 
-      if (file.exists()) {
-        try {
-          file.delete();
-        } 
-        catch (SecurityException e) {
-          ;//e.printStackTrace();
-        }
+    for (int i=0; i<GROUPS.length; i++) {
+
+      try { 
+        info = H5.H5Gget_info(H5fid); 
+      } 
+      catch (Throwable err) {
+        err.printStackTrace();
+        fail("H5.H5Gget_info: "+err);
       }
+      assertNotNull(info);
     }
+  }
 
-    @Before
-    public void createH5file() throws HDF5LibraryException, NullPointerException {
-      H5fid = H5.H5Fcreate(H5_FILE, HDF5Constants.H5F_ACC_TRUNC, 
-          HDF5Constants.H5P_DEFAULT, HDF5Constants.H5P_DEFAULT);
-      
-      int gid = -1;
-      
-      for (int i=0; i<GROUPS.length; i++) {
-          gid = _createGroup(H5fid, GROUPS[i]);
-          assertTrue(gid > 0);
-          try { H5.H5Gclose(gid); } catch (Exception ex) {}
+  @Test
+  public void testH5Gget_info_by_name() {
+    H5G_info_t info = null;
+
+    for (int i=0; i<GROUPS.length; i++) {
+      try { 
+        info = H5.H5Gget_info_by_name(H5fid, GROUPS[i], HDF5Constants.H5P_DEFAULT); 
+      } 
+      catch (Throwable err) {
+        err.printStackTrace();
+        fail("H5.H5Gget_info_by_name: "+err);
       }
-
-      H5.H5Fflush(H5fid, HDF5Constants.H5F_SCOPE_LOCAL);
+      assertNotNull(info);
     }
+  }
 
-    @After
-    public void deleteH5file() throws HDF5LibraryException {
-      if(H5fid > 0) {
-        H5.H5Fclose(H5fid);
+  @Test
+  public void testH5Gget_info_by_idx() {
+    H5G_info_t info = null;
+    for (int i=0; i<2; i++) {
+      try { 
+        info = H5.H5Gget_info_by_idx(H5fid, "/G1", HDF5Constants.H5_INDEX_NAME, 
+            HDF5Constants.H5_ITER_INC, i, HDF5Constants.H5P_DEFAULT); 
+      } 
+      catch (Throwable err) {
+        err.printStackTrace();
+        fail("H5.H5Gget_info_by_idx: "+err);
       }
-      _deleteFile(H5_FILE);
+      assertNotNull(info);
     }
-
-    @Test
-    public void testH5Gopen() {
-        for (int i=0; i<GROUPS.length; i++) {
-            int gid = _openGroup(H5fid, GROUPS[i]);
-            assertTrue(gid>0);
-            try { H5.H5Gclose(gid); } catch (Exception ex) {}
-        }
-    }
-
-    @Test
-    public void testH5Gget_create_plist() {
-        int gid=-1;
-        int pid=-1;
-
-        for (int i=0; i<GROUPS.length; i++) {
-            gid = _openGroup(H5fid, GROUPS[i]);
-            assertTrue(gid>0);
-            
-            try { 
-              pid = H5.H5Gget_create_plist(gid); 
-              }
-            catch (Throwable err) {
-              err.printStackTrace();
-                fail("H5.H5Gget_create_plist: "+err);
-            }
-            assertTrue(pid>0);
-            
-            try { H5.H5Gclose(gid); } catch (Exception ex) {}
-        }
-    }
-
-    @Test
-    public void testH5Gget_info() {
-        H5G_info_t info = null;
-
-        for (int i=0; i<GROUPS.length; i++) {
-
-        try { 
-          info = H5.H5Gget_info(H5fid); 
-          } 
-        catch (Throwable err) {
-          err.printStackTrace();
-            fail("H5.H5Gget_info: "+err);
-        }
-        assertNotNull(info);
-    }
-    }
-
-    @Test
-    public void testH5Gget_info_by_name() {
-        H5G_info_t info = null;
-
-        for (int i=0; i<GROUPS.length; i++) {
-        try { 
-          info = H5.H5Gget_info_by_name(H5fid, GROUPS[i], HDF5Constants.H5P_DEFAULT); 
-          } 
-        catch (Throwable err) {
-          err.printStackTrace();
-            fail("H5.H5Gget_info_by_name: "+err);
-        }
-        assertNotNull(info);
-    }
-    }
-
-    @Test
-    public void testH5Gget_info_by_idx() {
-        H5G_info_t info = null;
-        for (int i=0; i<2; i++) {
-        try { 
-          info = H5.H5Gget_info_by_idx(H5fid, "/G1", HDF5Constants.H5_INDEX_NAME, 
-                             HDF5Constants.H5_ITER_INC, i, HDF5Constants.H5P_DEFAULT); 
-          } 
-        catch (Throwable err) {
-          err.printStackTrace();
-            fail("H5.H5Gget_info_by_idx: "+err);
-        }
-        assertNotNull(info);
-      }
-    }
+  }
 
 //    @Test
 //    public void testH5Gget_obj_info_all() {
