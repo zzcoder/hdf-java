37301f5c7d7beeed824d02f444ed94fc99412b62
byrn <byrn@a541ca71-9318-0410-8328-a4f699e697c0>
2010-01-05 22:37:13 +0000

    Added more junit tests and corrected errors in implementation.
    Tested local linux

--- test/junit/TestH5T.java
+++ test/junit/TestH5T.java
@@ -1,5 +1,115 @@
 package test.junit;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+
+import ncsa.hdf.hdf5lib.H5;
+import ncsa.hdf.hdf5lib.HDF5Constants;
+import ncsa.hdf.hdf5lib.exceptions.HDF5Exception;
+import ncsa.hdf.hdf5lib.exceptions.HDF5LibraryException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 public class TestH5T {
+    private static final String H5_FILE = "test.h5";
+    int H5fid = -1;
+    int H5strdid = -1;
+
+    private final void _deleteFile(String filename) {
+        File file = new File(filename);
+
+        if (file.exists()) {
+            try {
+                file.delete();
+            }
+            catch (SecurityException e) {
+                ;// e.printStackTrace();
+            }
+        }
+    }
+
+    @Before
+    public void createH5file()
+            throws NullPointerException, HDF5Exception {
+        H5fid = H5.H5Fcreate(H5_FILE, HDF5Constants.H5F_ACC_TRUNC,
+                HDF5Constants.H5P_DEFAULT, HDF5Constants.H5P_DEFAULT);
+        assertTrue("H5.H5Fcreate",H5fid > 0);
+        H5strdid = H5.H5Tcopy(HDF5Constants.H5T_C_S1);
+        assertTrue("H5.H5Tcopy",H5strdid > 0);
+
+        H5.H5Fflush(H5fid, HDF5Constants.H5F_SCOPE_LOCAL);
+    }
+
+    @After
+    public void deleteH5file() throws HDF5LibraryException {
+        if (H5strdid >= 0)
+            H5.H5Tclose(H5strdid);
+        if (H5fid > 0) 
+            H5.H5Fclose(H5fid);
+
+        _deleteFile(H5_FILE);
+    }
+    
+    @Test
+    public void testH5Tget_class() throws Throwable, HDF5LibraryException {
+        int result = H5.H5Tget_class(H5strdid);
+        assertTrue("H5.H5Tget_class",result > 0);
+        String class_name = H5.H5Tget_class_name(result);
+        assertTrue("H5.H5Tget_class",class_name.compareTo("H5T_STRING")==0);
+    }
+    
+    @Test
+    public void testH5Tget_size() throws Throwable, HDF5LibraryException {
+        long dt_size = -1; 
+        try {
+            dt_size = H5.H5Tget_size(H5strdid);
+        }
+        catch (Throwable err) {
+            err.printStackTrace();
+            fail("testH5Tget_size:H5.H5Tget_size " + err);
+        }
+        assertTrue("testH5Tget_size",dt_size > 0);
+    }
+    
+    @Test
+    public void testH5Tset_size() throws Throwable, HDF5LibraryException {
+        long dt_size = 5; 
+        try {
+            H5.H5Tset_size(H5strdid, dt_size);
+        }
+        catch (Throwable err) {
+            err.printStackTrace();
+            fail("testH5Tset_size:H5.H5Tset_size " + err);
+        }
+        try {
+            dt_size = H5.H5Tget_size(H5strdid);
+        }
+        catch (Throwable err) {
+            err.printStackTrace();
+            fail("testH5Tget_size:H5.H5Tget_size " + err);
+        }
+        assertTrue("testH5Tget_size",dt_size == 5);
+    }
+    
+    @Test
+    public void testH5Tarray_create() throws Throwable, HDF5LibraryException {
+       int filetype_id = -1;
+       long[] adims = { 3, 5 };
+
+       try {
+           filetype_id = H5.H5Tarray_create(HDF5Constants.H5T_STD_I64LE, 2, adims);
+        }
+        catch (Throwable err) {
+            err.printStackTrace();
+            fail("testH5Tarray_create.H5Tarray_create " + err);
+        }
+        assertTrue("testH5Tarray_create",filetype_id > 0);
+        if (filetype_id >= 0)
+            H5.H5Tclose(filetype_id);
+    }
 
 }
--- test/junit/TestH5Tbasic.java
+++ test/junit/TestH5Tbasic.java
@@ -1,7 +1,7 @@
 package test.junit;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertFalse;
 import ncsa.hdf.hdf5lib.H5;
 import ncsa.hdf.hdf5lib.HDF5Constants;
 import ncsa.hdf.hdf5lib.exceptions.HDF5FunctionArgumentException;
@@ -10,111 +10,42 @@ import ncsa.hdf.hdf5lib.exceptions.HDF5LibraryException;
 import org.junit.Test;
 
 public class TestH5Tbasic {
-
-    @Test(expected = HDF5LibraryException.class)
-    public void testH5Tclose_invalid() throws Throwable, HDF5LibraryException {
-        H5.H5Tclose(-1);
+    
+    @Test
+    public void testH5Tcopy() throws Throwable, HDF5LibraryException {
+        int H5strdid = H5.H5Tcopy(HDF5Constants.H5T_C_S1);
+        assertTrue("H5.H5Tcopy",H5strdid > 0);
+        if (H5strdid >= 0)
+            H5.H5Tclose(H5strdid);
+    }
+    
+    @Test
+    public void testH5Tequal() throws Throwable, HDF5LibraryException {
+        int H5strdid = H5.H5Tcopy(HDF5Constants.H5T_C_S1);
+        assertTrue("H5.H5Tcopy",H5strdid > 0);
+        boolean teq = H5.H5Tequal(HDF5Constants.H5T_C_S1, H5strdid);
+        if (H5strdid >= 0)
+            H5.H5Tclose(H5strdid);
+        assertTrue("H5.H5Tequal",teq);
+    }
+ 
+    @Test(expected = HDF5FunctionArgumentException.class)
+    public void testH5Tequal_type_error() throws Throwable, HDF5LibraryException {
+        int H5strdid = H5.H5Tcopy(HDF5Constants.H5T_C_S1);
+        assertTrue("H5.H5Tcopy",H5strdid > 0);
+        boolean teq = H5.H5Tequal(HDF5Constants.H5T_INTEGER, H5strdid);
+        if (H5strdid >= 0)
+            H5.H5Tclose(H5strdid);
+        assertFalse("H5.H5Tequal",teq);
     }
 
-    @Test(expected = HDF5LibraryException.class)
-    public void testH5Tcreate_invalid()
-            throws Throwable, HDF5LibraryException {
-        H5.H5Tcreate(-1, 0);
+    @Test
+    public void testH5Tequal_not() throws Throwable, HDF5LibraryException {
+        int H5strdid = H5.H5Tcopy(HDF5Constants.H5T_STD_U64LE);
+        assertTrue("H5.H5Tcopy",H5strdid > 0);
+        boolean teq = H5.H5Tequal(HDF5Constants.H5T_IEEE_F32BE, H5strdid);
+        assertFalse("H5.H5Tequal",teq);
+        if (H5strdid >= 0)
+            H5.H5Tclose(H5strdid);
     }
-
-    @Test(expected = NullPointerException.class)
-    public void testH5Topen_null() throws Throwable, HDF5LibraryException, NullPointerException {
-        H5.H5Topen(-1, null, 0);
-    }
-
-    @Test(expected = HDF5LibraryException.class)
-    public void testH5Topen_invalid() throws Throwable, HDF5LibraryException, NullPointerException {
-        H5.H5Topen(-1, "Bogus", 0);
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testH5Tcommit_null() throws Throwable, HDF5LibraryException, NullPointerException {
-        H5.H5Tcommit(-1, null, 0, -1, -1, -1);
-    }
-
-    @Test(expected = HDF5LibraryException.class)
-    public void testH5Tcommit_invalid() throws Throwable, HDF5LibraryException, NullPointerException {
-        H5.H5Tcommit(-1, "Bogus", -1, -1, -1, -1);
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testH5Tget_pad_null() throws Throwable, HDF5LibraryException, NullPointerException {
-        H5.H5Tget_pad(-1, null);
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testH5Tget_fields_null() throws Throwable, HDF5LibraryException, NullPointerException {
-        H5.H5Tget_fields(-1, (long[])null);
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testH5Tget_member_index_null() throws Throwable, HDF5LibraryException, NullPointerException {
-        H5.H5Tget_member_index(-1, null);
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testH5Tinsert_null() throws Throwable, HDF5LibraryException, NullPointerException {
-        H5.H5Tinsert(-1, null, 0, 0);
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testH5Tset_tag_null() throws Throwable, HDF5LibraryException, NullPointerException {
-        H5.H5Tset_tag(-1, null);
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testH5Tget_member_value_null() throws Throwable, HDF5LibraryException, NullPointerException {
-        H5.H5Tget_member_value(-1, -1, (byte[])null);
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testH5Tget_array_dims_null() throws Throwable, HDF5LibraryException, NullPointerException {
-        H5.H5Tget_array_dims(-1, null);
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testH5Tenum_insert_name_null() throws Throwable, HDF5LibraryException, NullPointerException {
-        H5.H5Tenum_insert(-1, null, (byte[])null);
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testH5Tenum_insert_null() throws Throwable, HDF5LibraryException, NullPointerException {
-        H5.H5Tenum_insert(-1, "bogus", (byte[])null);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testH5Tenum_nameof_invalid() throws Throwable, HDF5LibraryException, NullPointerException {
-        H5.H5Tenum_nameof(-1, null, -1);
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testH5Tenum_nameof_value_null() throws Throwable, HDF5LibraryException, NullPointerException {
-        H5.H5Tenum_nameof(-1, null, 1);
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testH5Tenum_valueof_name_null() throws Throwable, HDF5LibraryException, NullPointerException {
-        H5.H5Tenum_valueof(-1, null, (byte[])null);
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testH5Tenum_valueof_null() throws Throwable, HDF5LibraryException, NullPointerException {
-        H5.H5Tenum_valueof(-1, "bogus", (byte[])null);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testH5Tarray_create_invalid() throws Throwable, HDF5LibraryException, NullPointerException {
-        H5.H5Tarray_create(-1, -1, null);
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testH5Tarray_create_value_null() throws Throwable, HDF5LibraryException, NullPointerException {
-        H5.H5Tarray_create(-1, 1, null);
-    }
-
 }
