8de74d56db22a20c2632d74c200ab1a91dfc9d37
byrn <byrn@a541ca71-9318-0410-8328-a4f699e697c0>
2010-01-14 15:58:44 +0000

    Corrected incorrect use of H5O_get_obj_info in H5Literate call within the H5.H5Gget_obj_info_all API.

--- trunk/test/junit/TestAll.java
+++ trunk/test/junit/TestAll.java
@@ -6,7 +6,8 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses( { TestH5.class, TestH5Eregister.class,
         TestH5Edefault.class, TestH5E.class, TestH5Fparams.class,
-        TestH5Fbasic.class, TestH5F.class, TestH5Gbasic.class, TestH5G.class,
+        TestH5Fbasic.class, TestH5F.class, 
+        TestH5Gbasic.class, TestH5G.class, TestH5Giterate.class,
         TestH5Sbasic.class, TestH5S.class, 
         TestH5Tparams.class, TestH5Tbasic.class, TestH5T.class, 
         TestH5Dparams.class
