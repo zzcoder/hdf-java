21693d4bb0835a5d63a607426543b9381431edc6
byrn <byrn@a541ca71-9318-0410-8328-a4f699e697c0>
2010-02-05 22:27:51 +0000

    Added H5Lget_info API
    Tested: local Linux

---
+++ trunk/test/hdf5lib/TestH5Lbasic.java
@@ -0,0 +1,121 @@
+package test.hdf5lib;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import ncsa.hdf.hdf5lib.H5;
+import ncsa.hdf.hdf5lib.HDF5Constants;
+import ncsa.hdf.hdf5lib.exceptions.HDF5LibraryException;
+import ncsa.hdf.hdf5lib.exceptions.HDF5SymbolTableException;
+import ncsa.hdf.hdf5lib.structs.H5L_info_t;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestH5Lbasic {
+    private static final boolean is16 = H5.isAPI16;
+    private static final String H5_FILE = "test/hdf5lib/h5ex_g_iterate.h5";
+    int H5fid = -1;
+
+    private final int _openGroup(int fid, String name) {
+        int gid = -1;
+        try {
+            if (is16)
+                gid = H5.H5Gopen(fid, name);
+            else
+                gid = H5.H5Gopen2(fid, name, HDF5Constants.H5P_DEFAULT);
+        }
+        catch (Throwable err) {
+            gid = -1;
+            err.printStackTrace();
+            fail("H5.H5Gcreate: " + err);
+        }
+
+        return gid;
+    }
+
+    @Before
+    public void openH5file()
+            throws HDF5LibraryException, NullPointerException {
+        try {
+            H5fid = H5.H5Fopen(H5_FILE, HDF5Constants.H5F_ACC_RDONLY,
+                HDF5Constants.H5P_DEFAULT);
+        }
+        catch (Throwable err) {
+            err.printStackTrace();
+            fail("H5.H5Fopen: openH5file: " + err);
+        }
+    }
+
+    @After
+    public void deleteH5file() throws HDF5LibraryException {
+        if (H5fid > 0) {
+            H5.H5Fclose(H5fid);
+        }
+    }
+
+    @Test
+    public void testH5Lexists() throws Throwable, HDF5LibraryException, NullPointerException {
+        boolean link_exists = false;
+        try {
+            link_exists = H5.H5Lexists(H5fid, "None", HDF5Constants.H5P_DEFAULT);
+        }
+        catch (Throwable err) {
+            err.printStackTrace();
+            fail("H5.H5Lexists: " + err);
+        }
+        assertFalse("H5Lexists ",link_exists);
+        try {
+            link_exists = H5.H5Lexists(H5fid, "DS1", HDF5Constants.H5P_DEFAULT);
+        }
+        catch (Throwable err) {
+            err.printStackTrace();
+            fail("H5.H5Lexists: " + err);
+        }
+        assertTrue("H5Lexists ",link_exists);
+        try {
+            link_exists = H5.H5Lexists(H5fid, "G1/DS2", HDF5Constants.H5P_DEFAULT);
+        }
+        catch (Throwable err) {
+            err.printStackTrace();
+            fail("H5.H5Lexists: " + err);
+        }
+        assertTrue("H5Lexists ",link_exists);
+    }
+
+    @Test(expected = HDF5SymbolTableException.class)
+    public void testH5Lget_info_not_exist() throws Throwable, HDF5LibraryException, NullPointerException {
+        H5.H5Lget_info(H5fid, "None", HDF5Constants.H5P_DEFAULT);
+    }
+
+    @Test
+    public void testH5Lget_info_dataset() throws Throwable, HDF5LibraryException, NullPointerException {
+        H5L_info_t link_info = null;
+        try {
+            link_info = H5.H5Lget_info(H5fid, "DS1", HDF5Constants.H5P_DEFAULT);
+        }
+        catch (Throwable err) {
+            err.printStackTrace();
+            fail("H5.H5Lget_info: " + err);
+        }
+        assertFalse("H5Lget_info ",link_info==null);
+        assertTrue("H5Lget_info link type",link_info.type==HDF5Constants.H5L_TYPE_HARD);
+   }
+
+    @Test
+    public void testH5Lget_info_hardlink() throws Throwable, HDF5LibraryException, NullPointerException {
+        H5L_info_t link_info = null;
+        try {
+            link_info = H5.H5Lget_info(H5fid, "L1", HDF5Constants.H5P_DEFAULT);
+        }
+        catch (Throwable err) {
+            err.printStackTrace();
+            fail("H5.H5Lget_info: " + err);
+        }
+        assertFalse("H5Lget_info ",link_info==null);
+        assertTrue("H5Lget_info link type",link_info.type==HDF5Constants.H5L_TYPE_HARD);
+        assertTrue("Link Address ",link_info.address_val_size>0);
+   }
+
+}
--- trunk/test/hdf5lib/TestH5Lparams.java
+++ trunk/test/hdf5lib/TestH5Lparams.java
@@ -1,10 +1,6 @@
 package test.hdf5lib;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 import ncsa.hdf.hdf5lib.H5;
-import ncsa.hdf.hdf5lib.HDF5Constants;
-import ncsa.hdf.hdf5lib.exceptions.HDF5FunctionArgumentException;
 import ncsa.hdf.hdf5lib.exceptions.HDF5LibraryException;
 
 import org.junit.Test;
@@ -19,6 +15,26 @@ public class TestH5Lparams {
     @Test(expected = NullPointerException.class)
     public void testH5Lget_val_null() throws Throwable, HDF5LibraryException, NullPointerException {
         H5.H5Lget_val(-1, null, 0);
+    }
+
+    @Test(expected = HDF5LibraryException.class)
+    public void testH5Lexists_invalid() throws Throwable, HDF5LibraryException {
+        H5.H5Lexists(-1, "Bogus", -1);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testH5Lexists_null() throws Throwable, HDF5LibraryException, NullPointerException {
+        H5.H5Lexists(-1, null, 0);
+    }
+
+    @Test(expected = HDF5LibraryException.class)
+    public void testH5Lget_info_invalid() throws Throwable, HDF5LibraryException {
+        H5.H5Lget_info(-1, "Bogus", -1);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testH5Lget_info_null() throws Throwable, HDF5LibraryException, NullPointerException {
+        H5.H5Lget_info(-1, null, 0);
     }
 
 }
