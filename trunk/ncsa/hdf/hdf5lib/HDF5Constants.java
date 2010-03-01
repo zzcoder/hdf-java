a3b72cca4c511dffc9c1891bd56220b53e2c3d10
byrn <byrn@a541ca71-9318-0410-8328-a4f699e697c0>
2010-03-01 20:28:42 +0000

    Implemented creation order tests on H5Lget_info_by_idx(), H5Lget_val_by_index(), H5Ldelete_by_index()
    Tested: local linux

--- trunk/ncsa/hdf/hdf5lib/HDF5Constants.java
+++ trunk/ncsa/hdf/hdf5lib/HDF5Constants.java
@@ -242,6 +242,7 @@ public class HDF5Constants {
     public static final int H5P_FILE_ACCESS_DEFAULT = H5P_FILE_ACCESS_DEFAULT();
     public static final int H5P_FILE_CREATE = H5P_FILE_CREATE();
     public static final int H5P_FILE_CREATE_DEFAULT = H5P_FILE_CREATE_DEFAULT();
+    public static final int H5P_GROUP_CREATE = H5P_GROUP_CREATE();
     public static final int H5P_LINK_CREATE = H5P_LINK_CREATE();
     public static final int H5P_LINK_ACCESS = H5P_LINK_ACCESS();
     public static final int H5P_NO_CLASS = H5P_NO_CLASS();
@@ -937,6 +938,8 @@ public class HDF5Constants {
     private static native final int H5P_FILE_CREATE();
 
     private static native final int H5P_FILE_CREATE_DEFAULT();
+
+    private static native final int H5P_GROUP_CREATE();
 
     private static native final int H5P_LINK_CREATE();
 
