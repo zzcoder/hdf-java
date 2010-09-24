7ae9cc519399be698c7faf662735fa1f7f5de261
xcao <xcao@a541ca71-9318-0410-8328-a4f699e697c0>
2010-09-24 22:14:33 +0000

    Remove Override

--- ncsa/hdf/object/nc2/NC2Dataset.java
+++ ncsa/hdf/object/nc2/NC2Dataset.java
@@ -351,7 +351,6 @@ public class NC2Dataset extends ScalarDS {
     }
 
     //Implementing DataFormat
-	@Override
 	public List getMetadata(int... attrPropList) throws Exception {
 		throw new UnsupportedOperationException("getMetadata(int... attrPropList) is not supported");
 	}
