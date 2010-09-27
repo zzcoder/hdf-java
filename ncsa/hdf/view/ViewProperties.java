3b1ff60de1638dda01e19c0ca90e8e6769465068
xcao <xcao@a541ca71-9318-0410-8328-a4f699e697c0>
2010-09-27 13:59:23 +0000

    change version to beta.

--- ncsa/hdf/view/ViewProperties.java
+++ ncsa/hdf/view/ViewProperties.java
@@ -36,7 +36,7 @@ public class ViewProperties extends Properties {
     public static final long serialVersionUID = HObject.serialVersionUID;
 
     /** the version of the HDFViewer */
-    public static final String VERSION = "2.7 snapshot";
+    public static final String VERSION = "2.7 Beta";
 
     /** the local property file name */
     private static final String USER_PROPERTY_FILE = ".hdfview"
