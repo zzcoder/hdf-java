708d650b14a314e5730fe7b8b8dcbe929699c705
xcao <xcao@a541ca71-9318-0410-8328-a4f699e697c0>
2010-12-28 21:54:09 +0000

    use short comments for unsupported data

--- ncsa/hdf/object/h5/H5CompoundDS.java
+++ ncsa/hdf/object/h5/H5CompoundDS.java
@@ -345,8 +345,7 @@ public class H5CompoundDS extends CompoundDS {
                         || H5.H5Tequal(atom_tid,
                                 HDF5Constants.H5T_STD_REF_DSETREG)) {
                     String[] nullValues = new String[(int) lsize[0]];
-                    String errorStr = "not supported in HDFView for "
-                            + H5Datatype.getDatatypeDescription(atom_tid);
+                    String errorStr = "*unsupported*";
                     for (int j = 0; j < lsize[0]; j++) {
                         nullValues[j] = errorStr;
                     }
