54514a55a74e7b92d415e01161e0424160f875b3
xcao <xcao@a541ca71-9318-0410-8328-a4f699e697c0>
2011-02-22 18:19:39 +0000

    Add Serializable to the new structure created for hdf5 1.8

--- trunk/ncsa/hdf/object/FileFormat.java
+++ trunk/ncsa/hdf/object/FileFormat.java
@@ -25,8 +25,6 @@ import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.MutableTreeNode;
 import javax.swing.tree.TreeNode;
 
-import ncsa.hdf.hdf5lib.exceptions.HDF5Exception;
-
 
 /**
  * FileFormat defines general interfaces for working with files whose data is
