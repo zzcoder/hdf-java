bd3d4f3f9b738130bcb7d2013836390bbe46eff1
byrn <byrn@a541ca71-9318-0410-8328-a4f699e697c0>
2011-06-20 19:18:43 +0000

    Update library and tests to latest HDF 4.2.6 version.
    Tested: local linux

--- ncsa/hdf/hdflib/HDFLibrary.java
+++ ncsa/hdf/hdflib/HDFLibrary.java
@@ -17,7 +17,7 @@ import java.io.File;
  *  This is the Java interface for the HDF 4.1 library.
  *  <p>
  *  This code is the called by Java programs to access the
- *  entry points of the HDF 4.1 library.
+ *  entry points of the HDF 4.2 library.
  *  Each routine wraps a single HDF entry point, generally with the
  *  arguments and return codes analogous to the C interface.
  * <p>
@@ -218,7 +218,7 @@ public class HDFLibrary
 {
     public final static String HDFPATH_PROPERTY_KEY = "ncsa.hdf.hdflib.HDFLibrary.hdflib";
 
-    private final static String JHI_VERSION= "2.5";
+    private final static String JHI_VERSION= "2.6";
     private static boolean isLibraryLoaded = false;
 
     static { loadH4Lib(); }
