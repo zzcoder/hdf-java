9fe8a1809beeeb1d036903da8f40790b969bb18a
byrn <byrn@a541ca71-9318-0410-8328-a4f699e697c0>
2012-02-01 16:41:01 +0000

    Update HDF4 version to 4.2.7 for gui tests rename Config folder to config to be consistent with all other projects
    Tested: local linux

--- trunk/test/uitest/TestHDFViewMenu.java
+++ trunk/test/uitest/TestHDFViewMenu.java
@@ -25,7 +25,7 @@ import org.junit.Test;
 public class TestHDFViewMenu {
     private static FrameFixture mainFrameFixture;
     private static String HDF5VERSION = "HDF5 1.8.9";
-    private static String HDF4VERSION = "HDF 4.2.6";
+    private static String HDF4VERSION = "HDF 4.2.7";
 
     private File createFile(String name, boolean hdf4_type) {
         String file_ext;
