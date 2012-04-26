fb67a749cb21c6bd58e7de49f174b27aaaf402d5
byrn <byrn@a541ca71-9318-0410-8328-a4f699e697c0>
2012-04-26 21:35:22 +0000

    Add intro object level examples. Fixed warnings discovered during build.

--- examples/intro/H5_ReadWrite.java
+++ examples/intro/H5_ReadWrite.java
@@ -7,14 +7,16 @@ import ncsa.hdf.hdf5lib.H5;
 import ncsa.hdf.hdf5lib.HDF5Constants;
 
 public class H5_ReadWrite {
-	private static String FILENAME = "dset.h5";
+	private static String FILENAME = "H5_ReadWrite.h5";
 	private static String DATASETNAME = "dset";
 	private static final int DIM_X = 4;
 	private static final int DIM_Y = 6;
 
 	private static void ReadWriteDataset() {
 		int file_id = -1;
+        int dataspace_id = -1;
 		int dataset_id = -1;
+        long[] dims = { DIM_X, DIM_Y };
 		int[][] dset_data = new int[DIM_X][DIM_Y];
 
 		// Initialize the dataset.
@@ -22,23 +24,33 @@ public class H5_ReadWrite {
 			for (int jndx = 0; jndx < DIM_Y; jndx++)
 				dset_data[indx][jndx] = indx * 6 + jndx + 1;
 
-		// Open an existing file.
-		try {
-			file_id = H5.H5Fopen(FILENAME, HDF5Constants.H5F_ACC_RDWR,
-					HDF5Constants.H5P_DEFAULT);
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
+        // Create a new file using default properties.
+        try {
+            file_id = H5.H5Fcreate(FILENAME, HDF5Constants.H5F_ACC_TRUNC,
+                    HDF5Constants.H5P_DEFAULT, HDF5Constants.H5P_DEFAULT);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
 
-		// Open an existing dataset.
-		try {
-			if (file_id >= 0)
-				dataset_id = H5.H5Dopen(file_id, "/" + DATASETNAME, HDF5Constants.H5P_DEFAULT);
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
+        // Create the data space for the dataset.
+        try {
+            dataspace_id = H5.H5Screate_simple(2, dims, null);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Create the dataset.
+        try {
+            if ((file_id >= 0) && (dataspace_id >= 0))
+                dataset_id = H5.H5Dcreate(file_id, "/" + DATASETNAME,
+                        HDF5Constants.H5T_STD_I32BE, dataspace_id,
+                        HDF5Constants.H5P_DEFAULT, HDF5Constants.H5P_DEFAULT, HDF5Constants.H5P_DEFAULT);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
 
 		// Write the dataset.
 		try {
