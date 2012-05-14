5f1e8bdca5ea3469182bf874f0f13af5267254b0
byrn <byrn@a541ca71-9318-0410-8328-a4f699e697c0>
2012-05-14 21:46:31 +0000

    Add no compare option to runtest. Change object misc test to use no compare runtest.

--- trunk/config/cmake/runTest.cmake
+++ trunk/config/cmake/runTest.cmake
@@ -57,21 +57,23 @@ ENDIF (NOT ${TEST_RESULT} STREQUAL ${TEST_EXPECT})
 
 MESSAGE (STATUS "COMMAND Error: ${TEST_ERROR}")
 
-IF (WIN32 AND NOT MINGW)
-  FILE (READ ${TEST_REFERENCE} TEST_STREAM)
-  FILE (WRITE ${TEST_REFERENCE} "${TEST_STREAM}")
-ENDIF (WIN32 AND NOT MINGW)
+IF (NOT TEST_SKIP_COMPARE)
+  IF (WIN32 AND NOT MINGW)
+    FILE (READ ${TEST_REFERENCE} TEST_STREAM)
+    FILE (WRITE ${TEST_REFERENCE} "${TEST_STREAM}")
+  ENDIF (WIN32 AND NOT MINGW)
 
-# now compare the output with the reference
-EXECUTE_PROCESS (
-    COMMAND ${CMAKE_COMMAND} -E compare_files ${TEST_OUTPUT} ${TEST_REFERENCE}
-    RESULT_VARIABLE TEST_RESULT
-)
+  # now compare the output with the reference
+  EXECUTE_PROCESS (
+      COMMAND ${CMAKE_COMMAND} -E compare_files ${TEST_OUTPUT} ${TEST_REFERENCE}
+      RESULT_VARIABLE TEST_RESULT
+  )
 
-# again, if return value is !=0 scream and shout
-IF (TEST_RESULT)
-  MESSAGE (FATAL_ERROR "Failed: The output of ${TEST_PROGRAM} did not match ${TEST_REFERENCE}")
-ENDIF (TEST_RESULT)
+  # again, if return value is !=0 scream and shout
+  IF (TEST_RESULT)
+    MESSAGE (FATAL_ERROR "Failed: The output of ${TEST_PROGRAM} did not match ${TEST_REFERENCE}")
+  ENDIF (TEST_RESULT)
+ENDIF (NOT TEST_SKIP_COMPARE)
 
 # everything went fine...
 MESSAGE ("Passed: The output of ${TEST_PROGRAM} matches ${TEST_REFERENCE}")
