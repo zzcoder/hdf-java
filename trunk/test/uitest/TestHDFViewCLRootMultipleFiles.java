58db5a9e4fd7d9b3c44e1cbfe1f3fec81defc723
byrn <byrn@a541ca71-9318-0410-8328-a4f699e697c0>
2012-11-19 21:07:30 +0000

    JAVA-1722: add command line gui tests and fix command line processing
    Tested: local

---
+++ trunk/test/uitest/TestHDFViewCLRootMultipleFiles.java
@@ -0,0 +1,139 @@
+package test.uitest;
+
+import static org.fest.swing.finder.WindowFinder.findFrame;
+import static org.fest.swing.launcher.ApplicationLauncher.application;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.util.regex.Pattern;
+
+import javax.swing.JFrame;
+
+import org.fest.swing.core.BasicRobot;
+import org.fest.swing.core.GenericTypeMatcher;
+import org.fest.swing.core.Robot;
+import org.fest.swing.edt.FailOnThreadViolationRepaintManager;
+import org.fest.swing.fixture.FrameFixture;
+import org.fest.swing.fixture.JMenuItemFixture;
+import org.fest.swing.fixture.JTabbedPaneFixture;
+import org.fest.swing.fixture.JTreeFixture;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestHDFViewCLRootMultipleFiles {
+    private static FrameFixture mainFrameFixture;
+    // the version of the HDFViewer
+    private static String VERSION = "2.99";
+
+
+    private void closeFile() {
+        try {
+            JMenuItemFixture fileMenuItem = mainFrameFixture.menuItemWithPath("File", "Close All");
+            mainFrameFixture.robot.waitForIdle();
+            
+            fileMenuItem.requireVisible();
+            fileMenuItem.click();
+            mainFrameFixture.robot.waitForIdle();
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        catch (AssertionError ae) {
+            ae.printStackTrace();
+        }
+    }
+    
+    private static void clearRemovePropertyFile() {
+        // the local property file name
+        // look for the property file at the use home directory
+        String fn = ".hdfview" + VERSION.substring(0, 3);
+        String uh = System.getProperty("user.home") + File.separator + fn;
+        String ud = System.getProperty("user.dir") + File.separator + fn;
+
+        File prop_file = new File(uh);
+        if (prop_file.exists()) {
+            prop_file.delete();
+        }
+        else {
+            prop_file = new File(ud);
+            if (prop_file.exists()) {
+                prop_file.delete();
+            }
+        }
+    }
+
+    @BeforeClass
+    public static void setUpOnce() {
+        clearRemovePropertyFile();
+        FailOnThreadViolationRepaintManager.install();
+        Robot robot = BasicRobot.robotWithNewAwtHierarchy();
+        String envvalue = System.getenv("PWD");
+        application("ncsa.hdf.view.HDFView").withArgs("-root", envvalue, "tattrintsize.h5", "tintsize.h5").start();
+        mainFrameFixture = findFrame(
+                new GenericTypeMatcher<JFrame>(JFrame.class) {
+                    protected boolean isMatching(JFrame frame) {
+                        return frame.getTitle().equals("HDFView "+VERSION)
+                                && frame.isShowing();
+                    }
+                }).withTimeout(10000).using(robot);
+        mainFrameFixture.robot.waitForIdle();
+        mainFrameFixture.requireVisible();
+    }
+
+    @AfterClass
+    public static void finishApplication() {
+
+        mainFrameFixture.robot.waitForIdle();
+        // mainFrameFixture.requireNotVisible();
+        mainFrameFixture.cleanUp();
+    }
+
+    @Test
+    public void verifyFileList() {
+        try {
+            JTreeFixture filetree = mainFrameFixture.tree().focus();
+            assertTrue("Verify filetree shows:", filetree.target.getRowCount() == 11);
+            assertTrue("Verify filetree has file", (filetree.valueAt(0)).compareTo("tattrintsize.h5") == 0);
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        catch (AssertionError ae) {
+            ae.printStackTrace();
+        }
+        finally {
+            try {
+                closeFile();
+            }
+            catch (Exception ex) {}
+        }
+    }
+
+    @Test
+    public void verifyrootDir() {
+        try {
+            String envvalue = System.getenv("PWD");
+            
+            JTabbedPaneFixture tabPane = mainFrameFixture.tabbedPane("tabpane").focus();
+            tabPane.selectTab("Log Info");
+            mainFrameFixture.robot.waitForIdle();
+            
+            tabPane.requireVisible();
+            Pattern pattern = Pattern.compile("^HDFView root - " + envvalue + ".*", Pattern.DOTALL);
+            mainFrameFixture.textBox("status").requireText(pattern);
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        catch (AssertionError ae) {
+            ae.printStackTrace();
+        }
+        finally {
+            try {
+                closeFile();
+            }
+            catch (Exception ex) {}
+        }
+    }
+}
