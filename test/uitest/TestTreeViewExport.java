512881d313d8831de05a4ce6c7c9170bb2b0ed1e
byrn <byrn@a541ca71-9318-0410-8328-a4f699e697c0>
2013-10-07 16:31:30 +0000

    Update version to release 2.10

--- test/uitest/TestTreeViewExport.java
+++ test/uitest/TestTreeViewExport.java
@@ -26,7 +26,7 @@ import org.junit.Test;
 public class TestTreeViewExport {
     private static FrameFixture mainFrameFixture;
     // the version of the HDFViewer
-    private static String VERSION = "2.99";
+    private static String VERSION = "2.10";
 
     private File openHDF5File(String name, int initrows) {
         File hdf_file = new File(name+".h5");
