65a39a3d4a25c8d57225d599c9e1e84e8f9db37f
byrn <byrn@a541ca71-9318-0410-8328-a4f699e697c0>
2013-10-07 16:35:53 +0000

    Update hdf5 version to release 1.8.13

--- trunk/test/uitest/TestHDFViewMenu.java
+++ trunk/test/uitest/TestHDFViewMenu.java
@@ -25,7 +25,7 @@ import org.junit.Test;
 
 public class TestHDFViewMenu {
     private static FrameFixture mainFrameFixture;
-    private static String HDF5VERSION = "HDF5 1.8.12";
+    private static String HDF5VERSION = "HDF5 1.8.13";
     private static String HDF4VERSION = "HDF 4.2.9";
     // the version of the HDFViewer
     private static String VERSION = "2.99";
