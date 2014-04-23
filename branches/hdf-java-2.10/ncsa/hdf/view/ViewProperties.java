7ef30522d6f3073fbcbcd9576b8c7fc9c3f322f0
byrn <byrn@a541ca71-9318-0410-8328-a4f699e697c0>
2014-04-23 18:28:50 +0000

    Correct version
    Tested: local linux

--- branches/hdf-java-2.10/ncsa/hdf/view/ViewProperties.java
+++ branches/hdf-java-2.10/ncsa/hdf/view/ViewProperties.java
@@ -44,7 +44,7 @@ public class ViewProperties extends Properties {
     private final static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(ViewProperties.class);
     
     /** the version of the HDFViewer */
-    public static final String  VERSION              = "2.99";
+    public static final String  VERSION              = "2.10.1";
 
     /** the local property file name */
     private static final String USER_PROPERTY_FILE   = ".hdfview" + VERSION;
