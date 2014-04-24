6fae8e4308d28624a77c444535478b802d5fcd5a
byrn <byrn@a541ca71-9318-0410-8328-a4f699e697c0>
2014-04-24 16:03:54 +0000

    JAVA-1834: Created test module and test framework. Lowercased all CMake commands.
    Tested: local linux

--- config/cmake/HDFJavaMacros.cmake
+++ config/cmake/HDFJavaMacros.cmake
@@ -2,27 +2,27 @@
 MACRO (HDFJAVA_SET_LIB_OPTIONS libtarget libname libtype)
   HDF_SET_LIB_OPTIONS (${libtarget} ${libname} ${libtype})
   #message (STATUS "Target: ${libtarget} is ${libname} libtype: ${libtype}")
-  IF (${libtype} MATCHES "SHARED")
-    IF (WIN32)
-      SET (LIBHDF_VERSION ${HDFJAVA_PACKAGE_VERSION_MAJOR})
-    ELSE (WIN32)
-      SET (LIBHDF_VERSION ${HDFJAVA_PACKAGE_VERSION})
-    ENDIF (WIN32)
+  if (${libtype} MATCHES "SHARED")
+    if (WIN32)
+      set (LIBHDF_VERSION ${HDFJAVA_PACKAGE_VERSION_MAJOR})
+    else (WIN32)
+      set (LIBHDF_VERSION ${HDFJAVA_PACKAGE_VERSION})
+    endif (WIN32)
     #message (STATUS "Version: ${LIBHDF_VERSION}")
     SET_TARGET_PROPERTIES (${libtarget} PROPERTIES VERSION ${LIBHDF_VERSION})
     SET_TARGET_PROPERTIES (${libtarget} PROPERTIES SOVERSION ${LIBHDF_VERSION})
-  ENDIF (${libtype} MATCHES "SHARED")
+  endif (${libtype} MATCHES "SHARED")
 
   #-- Apple Specific install_name for libraries
-  IF (APPLE)
-    OPTION (HDF_BUILD_WITH_INSTALL_NAME "Build with library install_name set to the installation path" OFF)
-    IF (HDF_BUILD_WITH_INSTALL_NAME)
+  if (APPLE)
+    option (HDF_BUILD_WITH_INSTALL_NAME "Build with library install_name set to the installation path" OFF)
+    if (HDF_BUILD_WITH_INSTALL_NAME)
       SET_TARGET_PROPERTIES (${libtarget} PROPERTIES
           LINK_FLAGS "-current_version ${HDFJAVA_PACKAGE_VERSION} -compatibility_version ${HDFJAVA_PACKAGE_VERSION}"
           INSTALL_NAME_DIR "${CMAKE_INSTALL_PREFIX}/lib"
           BUILD_WITH_INSTALL_RPATH ${HDF_BUILD_WITH_INSTALL_NAME}
       )
-    ENDIF (HDF_BUILD_WITH_INSTALL_NAME)
-  ENDIF (APPLE)
+    endif (HDF_BUILD_WITH_INSTALL_NAME)
+  endif (APPLE)
 
 ENDMACRO (HDFJAVA_SET_LIB_OPTIONS)
