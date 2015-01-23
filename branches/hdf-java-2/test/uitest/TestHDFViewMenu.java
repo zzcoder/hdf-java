e380358e6908975dea75b7b4470ba75926b1a688
byrn <byrn@a541ca71-9318-0410-8328-a4f699e697c0>
2015-01-23 16:19:49 +0000

    Update test to match updated hdf4 version.

--- branches/hdf-java-2/test/uitest/TestHDFViewMenu.java
+++ branches/hdf-java-2/test/uitest/TestHDFViewMenu.java
@@ -26,7 +26,7 @@ import org.junit.Test;
 public class TestHDFViewMenu {
     private static FrameFixture mainFrameFixture;
     private static String HDF5VERSION = "HDF5 1.8.15";
-    private static String HDF4VERSION = "HDF 4.2.10";
+    private static String HDF4VERSION = "HDF 4.2.11";
     // the version of the HDFViewer
     private static String VERSION = "2.99";
 
